1f2ec01276b38cdd89660ad876d0ea805c57e269
Lucas Jacinto <lucasriantrabalho15@gmail.com>
2026-06-25 12:48:27 -0300

    Adiciona Combobox, estrutura base e desenha retangulo

--- testes/codigo1_fornecido.py
+++ testes/codigo1_fornecido.py
@@ -2,6 +2,7 @@
 # Ao desenhar outra, apaga a anterior
 
 import tkinter as tk
+from tkinter import ttk
 
 def marca_inicio(event):
     global ini_x, ini_y
@@ -9,24 +10,46 @@ def marca_inicio(event):
     ini_y = event.y
 
 def atualiza_fim(event):
-    global fim_x, fim_y
+    global fim_x, fim_y, id_figura_temp
     fim_x = event.x
     fim_y = event.y
-    canvas.delete("all")
-    canvas.create_line(ini_x, ini_y, fim_x, fim_y)
+    
+    ferramenta = combobox_ferramenta.get()
+    
+    if id_figura_temp:
+        canvas.delete(id_figura_temp)
+    
 
+    if ferramenta == "Linha":
+        id_figura_temp = canvas.create_line(ini_x, ini_y, fim_x, fim_y, dash=(4, 4))
+        
+    elif ferramenta == "Retângulo":
+        id_figura_temp = canvas.create_rectangle(ini_x, ini_y, fim_x, fim_y, dash=(4, 4))
+
+def finaliza_desenho(event):
+    global fim_x, fim_y, id_figura_temp
+    fim_x = event.x
+    fim_y = event.y
+    
 
 root = tk.Tk()
+
+opcoes = ["Linha", "Retângulo", "Oval"]
+combobox_ferramenta = ttk.Combobox(root, values=opcoes, state="readonly")
+combobox_ferramenta.set("Linha") # Define "Linha" como padrão ao abrir
+combobox_ferramenta.pack(pady=5)
 
 canvas = tk.Canvas(root, bg='white', width=600, height=600)
 canvas.pack()
 
-ini_x = None  # coordenadas do ponto inicial da linha
+ini_x = None
 ini_y = None
 fim_x = None
 fim_y = None
+id_figura_temp = None
 canvas.bind('<ButtonPress-1>', marca_inicio)
 canvas.bind('<B1-Motion>', atualiza_fim)
-#canvas.bind('<ButtonRelease-1>', reset)
+canvas.bind('<ButtonRelease-1>', finaliza_desenho)
+
 
 root.mainloop()
