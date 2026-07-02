ba9c7a2750782e79cc242c6f3fde84819eba7acd
GustavoTavares2410 <gugujogosps4@gmail.com>
2026-07-02 02:54:52 -0300

    Update figuras.py Definição dos Métodos
    Definição dos métodos obrigatórios e Implementação da Classe Rabisco

--- src/PaintBrush/figuras.py
+++ src/PaintBrush/figuras.py
@@ -1,1 +1,31 @@
+from abc import ABC, abstractmethod
 
+class Figura(ABC):
+    def __init__(self, cor_borda="black", cor_preenchimento=""):
+        self.cor_borda = cor_borda
+        self.cor_preenchimento = cor_preenchimento
+    @abstractmethod
+    def atualizar(self, x, y):
+        pass
+    @abstractmethod
+    def desenhar(self, canvas, tracejado=False):
+        pass
+    @abstractmethod
+    def esta_incompleta(self):
+        pass
+
+class Rabisco(Figura):
+    def __init__(self, x, y, cor_borda="black"):
+        super().__init__(cor_borda, cor_preenchimento="")
+        self.pontos = [(x, y)]
+      
+    def atualizar(self, x, y):
+        self.pontos.append((x, y))
+
+    def desenhar(self, canvas, tracejado=False):
+        if len(self.pontos) > 1:
+            dash_pattern = (4, 2) if tracejado else None     
+            canvas.create_line(self.pontos, fill=self.cor_borda, dash=dash_pattern)
+          
+    def esta_incompleta(self):
+        return len(self.pontos) <= 1
