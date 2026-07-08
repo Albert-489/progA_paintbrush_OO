350250945f420c333067a308486b77eb54552e9f
Albert-489 <albertfullstack07@gmail.com>
2026-07-08 20:03:13 -0300

    Botão sair, Canvas em tela cheia e novas cores

--- src/PaintBrush/view/interface.py
+++ src/PaintBrush/view/interface.py
@@ -5,6 +5,7 @@ class Interface:
     def __init__(self, root):
         self.root = root
         self.root.title("Mini Paint")
+        self.root.attributes('-fullscreen', True)
         
         frame_controles = tk.Frame(self.root)
         frame_controles.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
@@ -13,6 +14,9 @@ class Interface:
         self.btn_abrir = ttk.Button(frame_controles, text="Abrir")
         self.btn_abrir.pack(side=tk.LEFT, padx=5)
         
+        sair_botao = ttk.Button(frame_controles, text='Sair do programa', command= lambda: quit())
+        sair_botao.pack(side=tk.RIGHT, padx=5)
+
         ttk.Label(frame_controles, text='Ferramenta:').pack(side=tk.LEFT, padx=5)
         self.ferramenta_var = tk.StringVar()
         self.combobox_ferramenta = ttk.Combobox(frame_controles, textvariable=self.ferramenta_var, state="readonly", width=12)
@@ -23,18 +27,18 @@ class Interface:
         ttk.Label(frame_controles, text='Borda:').pack(side=tk.LEFT, padx=5)
         self.cor_borda_var = tk.StringVar()
         self.combobox_borda = ttk.Combobox(frame_controles, textvariable=self.cor_borda_var, state="readonly", width=10)
-        self.combobox_borda['values'] = ('Preto', 'Vermelho', 'Azul', 'Verde', 'Amarelo')
-        self.combobox_borda.set('Preto')
+        self.combobox_borda['values'] = ('Black', 'Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Pink', 'Brown', 'Cyan', 'Grey')
+        self.combobox_borda.set('Black')
         self.combobox_borda.pack(side=tk.LEFT, padx=5)
         
         ttk.Label(frame_controles, text='Preenchimento:').pack(side=tk.LEFT, padx=5)
         self.cor_preenchimento_var = tk.StringVar()
         self.combobox_preenchimento = ttk.Combobox(frame_controles, textvariable=self.cor_preenchimento_var, state="readonly", width=10)
-        self.combobox_preenchimento['values'] = ('Nenhum', 'Branco', 'Vermelho', 'Azul', 'Verde', 'Amarelo')
+        self.combobox_preenchimento['values'] = ('Nenhum', 'White', 'Red', 'Blue', 'Green', 'Yellow', 'Orange', 'Pink', 'Brown', 'Cyan', 'Grey')
         self.combobox_preenchimento.set('Nenhum')
         self.combobox_preenchimento.pack(side=tk.LEFT, padx=5)
         
-        self.canvas = tk.Canvas(self.root, bg='white', width=600, height=600)
+        self.canvas = tk.Canvas(self.root, bg='white', width=1920, height=1080)
         self.canvas.pack(side=tk.BOTTOM, padx=5, pady=5)
 
         
