8c96c1184cb5a3a6ed67ade04d3a05cb68268d45
GustavoTavares2410 <gugujogosps4@gmail.com>
2026-07-14 07:29:47 -0300

    Update desenho.py Alteração no arquivo desenho.py
    Muda figura selecionada para uma lista (figuras selecionadas), e adicionar os métodos para mover as figuras no índice da lista (para frente/trás)

--- src/PaintBrush/model/desenho.py
+++ src/PaintBrush/model/desenho.py
@@ -1,21 +1,35 @@
 import json
 from model.figuras import figura_from_dict
+
 class Desenho:
     def __init__(self):
         self.figuras_desenhadas = []
         self.figura_atual = None
-        self.figura_selecionada = None
+        self.figuras_selecionadas = []
 
     def remover_figura(self, figura):
         if figura in self.figuras_desenhadas:
             self.figuras_desenhadas.remove(figura)
-        if self.figura_selecionada is figura:
-            self.figura_selecionada = None
+        if figura in self.figuras_selecionadas:
+            self.figuras_selecionadas.remove(figura)
 
     def adicionar_figura(self):
         if self.figura_atual and not self.figura_atual.esta_incompleta():
             self.figuras_desenhadas.append(self.figura_atual)
         self.figura_atual = None
+
+    def mover_para_frente(self):
+        for figura in list(self.figuras_selecionadas):
+            if figura in self.figuras_desenhadas:
+                self.figuras_desenhadas.remove(figura)
+                self.figuras_desenhadas.append(figura)
+
+    def mover_para_tras(self):
+        for figura in reversed(list(self.figuras_selecionadas)):
+            if figura in self.figuras_desenhadas:
+                self.figuras_desenhadas.remove(figura)
+                self.figuras_desenhadas.insert(0, figura)
+
     def salvar_para_arquivo(self, caminho):
         dados = [figura.to_dict() for figura in self.figuras_desenhadas]
         with open(caminho, 'w', encoding='utf-8') as f:
@@ -26,6 +40,7 @@ class Desenho:
             dados = json.load(f)
         
         self.figuras_desenhadas = []
+        self.figuras_selecionadas = []
         for item in dados:
             figura = figura_from_dict(item)
             if figura:
